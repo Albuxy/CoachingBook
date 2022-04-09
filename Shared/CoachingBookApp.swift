9cb8f916f36ff87e281904166ca4b4609a7a04ae
Alba Torra <alba.torra01@estudiant.upf.edu>
2022-04-09 22:37:26 +0200

    Initial Commit

---
+++ Shared/CoachingBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoachingBookApp.swift
+//  Shared
+//
+//  Created by Alba Torra Di Capua on 9/4/22.
+//
+
+import SwiftUI
+
+@main
+struct CoachingBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
