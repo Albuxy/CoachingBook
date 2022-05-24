36ce8ee7e45d9825adf485308a0dd2a764e87eaa
Alba Torra <alba.torra01@estudiant.upf.edu>
2022-05-24 16:56:04 +0200

    General information attendance card

---
+++ Shared/Screens/Home/Cards/PrepareMatchCard.swift
@@ -0,0 +1,73 @@
+//
+//  PrepareMatchCard.swift
+//  CoachingBook (iOS)
+//
+//  Created by Alba Torra Di Capua on 24/5/22.
+//
+
+import SwiftUI
+
+struct PrepareMatchCard: View {
+
+    @State var navigateToPreparation = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5){
+            HStack(spacing: 30) {
+                ZStack {
+                    Circle()
+                        .stroke(lineWidth: 3.0)
+                        .foregroundColor(Color.init(hex: "456F6C"))
+                    Circle()
+                        .trim(from: 0.0, to: CGFloat(0.2))
+                        .stroke(style: StrokeStyle(lineWidth: 3.0, lineCap: .round, lineJoin: .round))
+                        .foregroundColor(.white)
+                    Text("\(Int(0.2 * 100))" + " %")
+                        .font(.system(size: 18))
+                        .foregroundColor(.white)
+                        .bold()
+                }
+                .frame(width: 60, height: 60)
+                VStack(alignment: .leading, spacing: 10){
+                    Text("home_preparation_progress".localized(LocalizationService.shared.language))
+                        .font(.system(size: 14))
+                        .foregroundColor(.white)
+                    Text("home_preparation_match".localized(LocalizationService.shared.language))
+                        .font(.system(size: 18))
+                        .bold()
+                        .foregroundColor(.white)
+                }
+                .frame(width: 180, height: 100, alignment: .leading)
+            }
+            .frame(width: 200, height: 100,alignment: .leading)
+            NavigationLink(
+                destination: DashboardOnBoardingView(),
+              isActive: $navigateToPreparation,
+              label: {
+                  ButtonWithNavigation(boolean: $navigateToPreparation,
+                                       title: "home_continue_preparation",
+                                       color: Color.init(hex: "6AA5A1"))
+              }
+            )
+        }
+        .padding(.leading, 30)
+        .frame(width: 303, height: 150, alignment: .leading)
+        .background(
+            RoundedRectangle(cornerRadius: 5)
+                .strokeBorder(Color.clear, lineWidth: 1)
+                .background(
+                    Image("blueBackground")
+                        .resizable()
+                        .frame(width: 303, height: 160)
+                        .cornerRadius(15)
+                )
+        )
+        
+    }
+}
+
+struct PrepareMatchCard_Previews: PreviewProvider {
+    static var previews: some View {
+        PrepareMatchCard()
+    }
+}
--- Shared/Screens/Home/Cards/PrepareTrainingCard.swift
+++ Shared/Screens/Home/Cards/PrepareTrainingCard.swift
@@ -12,12 +12,12 @@ struct PrepareTrainingCard: View {
     @State var navigateToPreparation = false
     
     var body: some View {
-        VStack(alignment: .trailing, spacing: 5){
+        VStack(alignment: .leading, spacing: 5){
             HStack(spacing: 30) {
                 ZStack {
                     Circle()
                         .stroke(lineWidth: 3.0)
-                        .foregroundColor(.black)
+                        .foregroundColor(Color.init(hex: "74572B"))
                     Circle()
                         .trim(from: 0.0, to: CGFloat(0.6))
                         .stroke(style: StrokeStyle(lineWidth: 3.0, lineCap: .round, lineJoin: .round))
@@ -39,38 +39,18 @@ struct PrepareTrainingCard: View {
                 }
                 .frame(width: 180, height: 100, alignment: .leading)
             }
-            .padding(.leading, 30)
             .frame(width: 200, height: 100,alignment: .leading)
             NavigationLink(
               destination: DashboardOnBoardingView(),
               isActive: $navigateToPreparation,
               label: {
-                  Button {
-                      navigateToPreparation.toggle()
-                  } label: {
-                      HStack{
-                          Text("Continue preparation")
-                              .font(.system(size: 14))
-                              .bold()
-                              .foregroundColor(Color("brownLightColor"))
-                          Image(systemName: "arrowtriangle.right.fill")
-                              .resizable()
-                              .frame(width: 8, height: 8)
-                              .foregroundColor(Color("brownLightColor"))
-                      }
-                      .padding([.top, .bottom], 8)
-                      .padding([.leading, .trailing], 10)
-                      .background(
-                          RoundedRectangle(cornerRadius: 15)
-                              .strokeBorder(Color.clear, lineWidth: 1)
-                              .background(
-                                  RoundedRectangle(cornerRadius: 10).fill(Color.white)
-                              )
-                      )
-                  }
+                  ButtonWithNavigation(boolean: $navigateToPreparation,
+                                       title: "home_continue_preparation",
+                                       color: Color("brownLightColor"))
               }
             )
         }
+        .padding(.leading, 30)
         .frame(width: 303, height: 150, alignment: .leading)
         .background(
             RoundedRectangle(cornerRadius: 5)
@@ -83,6 +63,39 @@ struct PrepareTrainingCard: View {
                 )
         )
         
+    }
+}
+
+struct ButtonWithNavigation: View {
+    
+    @Binding var boolean: Bool
+    var title: String
+    var color: Color
+    
+    var body: some View{
+        Button {
+            boolean.toggle()
+        } label: {
+            HStack{
+                Text(title.localized(LocalizationService.shared.language))
+                    .font(.system(size: 14))
+                    .bold()
+                    .foregroundColor(color)
+                Image(systemName: "arrowtriangle.right.fill")
+                    .resizable()
+                    .frame(width: 8, height: 8)
+                    .foregroundColor(color)
+            }
+            .padding([.top, .bottom], 8)
+            .padding([.leading, .trailing], 10)
+            .background(
+                RoundedRectangle(cornerRadius: 15)
+                    .strokeBorder(Color.clear, lineWidth: 1)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10).fill(Color.white)
+                    )
+            )
+        }
     }
 }
 
