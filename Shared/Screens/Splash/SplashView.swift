d0af25bbcc9c4c845eaebd3d120ff928d2789dc2
Alba Torra <alba.torra01@estudiant.upf.edu>
2022-05-31 19:50:53 +0200

    Animation with json added

--- Shared/Screens/Splash/SplashView.swift
+++ Shared/Screens/Splash/SplashView.swift
@@ -6,10 +6,49 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct SplashView: View {
+    
+    @State var showlogin = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ZStack(alignment: Alignment(horizontal: .center, vertical: .top)){
+                AnimationSplashView(hasFinished: $showlogin)
+                    .background(Color.white)
+            }.navigate(to: LoginView(), when: $showlogin)
+        }
+    }
+}
+
+struct AnimationSplashView: UIViewRepresentable {
+    typealias UIViewType = UIView
+    
+    @Binding var hasFinished: Bool
+    
+    func makeUIView(context: UIViewRepresentableContext<AnimationSplashView>) -> UIView {
+        let view = UIView(frame: .zero)
+        
+        let animationView = AnimationView()
+        animationView.animation = Animation.named("splashAnimation")
+        animationView.contentMode = .scaleAspectFit
+        animationView.play { (finished) in
+            hasFinished.toggle()
+        }
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+        ])
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<AnimationSplashView>) {
+        //Nothing
     }
 }
 
